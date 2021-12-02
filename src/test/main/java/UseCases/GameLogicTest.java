186cbcaf8984da03f2a1a3f6818c2f278d0ee7f0
apple8ball <apple8ball@gmail.com>
2021-12-02 00:24:19 -0500

    Created test for GameLogic

---
+++ src/test/main/java/UseCases/GameLogicTest.java
@@ -0,0 +1,43 @@
+package test.main.java.UseCases;
+
+import junit.framework.TestCase;
+
+import javax.swing.*;
+import java.util.Iterator;
+
+public class GameLogicTest extends TestCase {
+
+    public void testUpdate() {
+    }
+
+    public void testSetUserName() {
+    }
+
+    public void testIterator() {
+        GameLogic gl = new GameLogic();
+        Iterator<JPanel> it = gl.iterator();
+        JPanel item;
+        int count = 0;
+        int nextCount = 0;
+
+        while (it.hasNext()){
+            item = it.next();
+            count++;
+        }
+
+        while (it.hasNext()){
+            item = it.next();
+            nextCount++;
+        }
+
+        /**
+         * The iterator should have the same number of items
+         * independent of whether it is a first or next run.
+         */
+        assertEquals(count, nextCount);
+
+    }
+
+    public void testMain() {
+    }
+}
